5d482280d77a24120e6e973772d00a2d41190eed
Habiba2030 <habiba.msale7@gmail.com>
2023-12-06 13:30:10 +0200

    first_commit

---
+++ Emt7an/Login.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Emt7an
+{
+    
+    /// <summary>
+    /// Interaction logic for Login.xaml
+    /// </summary>
+    public partial class Login : Page
+    {
+        desktop_systemEntities db = new desktop_systemEntities();
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                USSER u = new USSER();
+                if (user_name.Text != null && pass.Text != null)
+                {
+
+                    u = db.USSERs.First(x => x.US_NAME == user_name.Text && x.US_PASS == pass.Text);
+                    MessageBox.Show("Signed in");
+                    Profile profile = new Profile(u);
+                    this.NavigationService.Navigate(profile);
+
+                }
+                else
+                {
+                    MessageBox.Show("Must enter data");
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Wrong data");
+            }
+        }
+    }
+}
---
+++ Emt7an/Profile.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Emt7an
+{
+    /// <summary>
+    /// Interaction logic for Profile.xaml
+    /// </summary>
+    public partial class Profile : Page
+    {
+        public Profile(USSER u)
+        {
+            InitializeComponent();
+            u.US_NAME = user1.Text;
+            u.US_PASS = password1.Password;
+            u.US_AGE = int.Parse(ag1.Text);
+            u.US_GENDER = gender1.Text;
+            u.US_CITY = city1.Text;
+            u.US_PHONENUM = num1.Text;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Login login = new Login();
+            this.NavigationService.Navigate(login);
+        }
+    }
+}
